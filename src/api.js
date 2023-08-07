b617f9e943ef9d66b023c16c0000c499ce1717a6
skyisrrna <skyisrrna@naver.com>
2023-08-07 13:17:19 +0900

    react

---
+++ src/api.js
@@ -0,0 +1,36 @@
+const BASE_PATH =
+    process.env.NODE_ENV === "production"
+        ? "https://port-0-yj-project4-back-3prof2lll07acig.sel4.cloudtype.appnp"
+        : "http://localhost:8080";
+
+export async function rentalNotices() {
+    return await fetch(`${BASE_PATH}/api/rental/notice`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        credentials: "include",
+    }).then((res) => res.json());
+}
+
+export async function rentalNoticeDetail(props) {
+    const id = props.queryKey[1];
+    return await fetch(`${BASE_PATH}/api/rental/notice-detail/${id}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        credentials: "include",
+    }).then((res) => res.json());
+}
+
+export async function rentalNoticeWrite(props) {
+    return await fetch(`${BASE_PATH}/api/rental/notice-write`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify(props),
+    }).then((res) => res.json());
+}
